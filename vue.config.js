c11ed74cb80764e6d9ab5871941e010f19dbf31e
laixingyou <1156663717@qq.com>
2020-04-21 17:27:03 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    lintOnSave: false,
+    configureWebpack: {
+        resolve: {
+            alias: {
+
+            }
+        }
+    },
+    css: {
+        extract: false
+    }
+
+}
